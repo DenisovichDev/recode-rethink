0ef1b694334fbbaac44bcc8696ee352e6563874b
Ivan-Denisovich-py <bhaswarishi2908@gmail.com>
2022-06-24 16:25:56 +0530

    Wrapping up

--- gravel/rethink/sketch.js
+++ gravel/rethink/sketch.js
@@ -1,49 +1,49 @@
-let w = 30;
-let marginX = 4 * w;
-let marginY = w / 2;
-let cols, rows;
-let boxes = [];
-let gridInfo = [];
+let w = 30
+let marginX = 4 * w
+let marginY = w / 2
+let cols, rows
+let boxes = []
+let gridInfo = []
 
-let count = 0;
-let capture = true;
+let count = 0
+// let capture = true;
 
 function setup() {
-    createCanvas(637, 800);
-    bg = color(0);
-    background(bg);
-    cols = floor((width - 2 * marginX) / w);
-    rows = floor((height - 2 * marginY) / w);
+    createCanvas(637, 800)
+    bg = color(0)
+    background(bg)
+    cols = floor((width - 2 * marginX) / w)
+    rows = floor((height - 2 * marginY) / w)
 
     for (let j = 0; j < rows; j++) {
         for (let i = 0; i < cols; i++) {
-            let index = i + j * cols;
-            boxes.push(new Box(i, j, w));
+            let index = i + j * cols
+            boxes.push(new Box(i, j, w))
             gridInfo[index] = [
                 createVector(boxes[index].x, boxes[index].y),
                 boxes[index].rotation,
-            ];
+            ]
         }
     }
 
-    strokeWeight(2);
-    stroke(150);
-    noFill();
+    strokeWeight(2)
+    stroke(150)
+    noFill()
 
-    frameRate(4);
+    frameRate(4)
 }
 
 function draw() {
-    background(bg);
+    background(bg)
 
-    translate(width / 2, height / 2);
-    scale(1.2);
-    translate(-width / 2, -height / 2);
+    translate(width / 2, height / 2)
+    scale(1.2)
+    translate(-width / 2, -height / 2)
     for (let j = 0; j < rows; j++) {
         for (let i = 0; i < cols; i++) {
-            let index = i + j * cols;
-            boxes[index].draw();
-            boxes[index].update();
+            let index = i + j * cols
+            boxes[index].draw()
+            boxes[index].update()
         }
     }
 
@@ -55,9 +55,9 @@ function draw() {
 
     if (capture && !(boxes[0].lerpFrac >= 1)) {
         // if (frameCount % 3 === 0) {
-        saveCanvas("output-" + count, "png");
-        count++;
+        saveCanvas("output-" + count, "png")
+        count++
         // }
     }
-    console.log(frameCount, count);
+    console.log(frameCount, count)
 }
